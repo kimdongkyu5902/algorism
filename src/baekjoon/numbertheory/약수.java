73b8be831024fd85deaf77b01b961a203508cf79
dongkyu <kimdongkyu5902@hanmail.net>
2021-06-27 13:33:10 +0900

    백준 > 정수론 및 조합론 > 약수
    약수의 특성상 정렬 후,
    맨앞 맨뒤를 곱하면 배수의 크기가 된다.
    약수가 1개일 땐, 제곱을 하면 배수가 된다.

---
+++ src/baekjoon/numbertheory/약수.java
@@ -0,0 +1,40 @@
+package baekjoon.numbertheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.
+ * <p>
+ * 입력
+ * 첫째 줄에 N의 진짜 약수의 개수가 주어진다. 이 개수는 50보다 작거나 같은 자연수이다. 둘째 줄에는 N의 진짜 약수가 주어진다. 1,000,000보다 작거나 같고, 2보다 크거나 같은 자연수이고, 중복되지 않는다.
+ * <p>
+ * 출력
+ * 첫째 줄에 N을 출력한다. N은 항상 32비트 부호있는 정수로 표현할 수 있다.
+ */
+public class 약수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        if (arr.length == 1) {
+            int a = arr[0];
+            System.out.println(a * a);
+        } else {
+            int a = arr[0];
+            int b = arr[N - 1];
+            System.out.println(a * b);
+        }
+    }
+}
