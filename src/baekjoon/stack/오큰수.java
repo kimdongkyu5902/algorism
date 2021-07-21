b6f31ac9009a3a259b2c5fc6e43874e672cb227e
dongkyu <kimdongkyu5902@hanmail.net>
2021-07-21 23:30:51 +0900

    백준 > 스택 > 오큰수
    전체 배열에 대하여,
    현재보다 처음 큰 수를 찾는다고 하면
    n번의 반복문 안에 n-1번 반복문이 필요하다.
    -> 메모리 부족
    
    수열을 탐색하면서 현재 원소가 이전의 원소보다 작을 때 까지 stack에 수열의 index를 추가(push) 하는 것이다. 그리고 만약 현재 원소가 스택의 top 원소를 인덱스로 하는 수열의 원소보다 크게 될 경우 stack의 원소를 pop하면서 해당 인덱스에 해당하는 원소들을 현재 원소로 바꿔주는 것이다.
    마지막에 불가능한 인덱스들을 -1로 채워줌
    -> 생성되는 메모리 최적화 가능

---
+++ src/baekjoon/stack/오큰수.java
@@ -0,0 +1,63 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다. Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+ *
+ * 예를 들어, A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다. A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
+ *
+ * 입력
+ * 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+ *
+ * 출력
+ * 총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
+ */
+public class 오큰수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] seq = new int[N];
+        int[] stack = new int[N];	// 스택처럼 쓸 배열
+        int top = -1;	// 스택의 맨 위 원소를 가리키는 변수
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            /*
+             * 스택이 비어있지 않으면서 (= top 이 -1이 아닐 경우)
+             * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
+             * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
+             * 해당 인덱스의 값을 현재 원소로 바꿔준다.
+             */
+            while (top != - 1 && seq[stack[top]] < seq[i]) {
+                seq[stack[top]] = seq[i];
+                top--;
+            }
+            top++;
+            stack[top] = i;
+        }
+
+        /*
+         * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
+         * -1로 초기화한다.
+         */
+        for(int i = top; i >= 0; i--) {
+            seq[stack[i]] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int v : seq) {
+            sb.append(v).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
