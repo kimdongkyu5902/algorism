29d12b809d3a53de43e85836b40eebd9f52b0f0d
dongkyu <kimdongkyu5902@hanmail.net>
2021-07-25 15:20:54 +0900

    백준 > 큐,덱 > 프린터큐
    초기위치와 중요도를 가지고 있는 큐를 사용하여
    출력 중요도를 검사하며, 만족하는 조건을 탐색

---
+++ src/baekjoon/stack/프린터큐.java
@@ -0,0 +1,83 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+/**
+ * 여러분도 알다시피 여러분의 프린터 기기는 여러분이 인쇄하고자 하는 문서를 인쇄 명령을 받은 ‘순서대로’, 즉 먼저 요청된 것을 먼저 인쇄한다. 여러 개의 문서가 쌓인다면 Queue 자료구조에 쌓여서 FIFO - First In First Out - 에 따라 인쇄가 되게 된다. 하지만 상근이는 새로운 프린터기 내부 소프트웨어를 개발하였는데, 이 프린터기는 다음과 같은 조건에 따라 인쇄를 하게 된다.
+ *
+ * 현재 Queue의 가장 앞에 있는 문서의 ‘중요도’를 확인한다.
+ * 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 이 문서를 인쇄하지 않고 Queue의 가장 뒤에 재배치 한다. 그렇지 않다면 바로 인쇄를 한다.
+ * 예를 들어 Queue에 4개의 문서(A B C D)가 있고, 중요도가 2 1 4 3 라면 C를 인쇄하고, 다음으로 D를 인쇄하고 A, B를 인쇄하게 된다.
+ *
+ * 여러분이 할 일은, 현재 Queue에 있는 문서의 수와 중요도가 주어졌을 때, 어떤 한 문서가 몇 번째로 인쇄되는지 알아내는 것이다. 예를 들어 위의 예에서 C문서는 1번째로, A문서는 3번째로 인쇄되게 된다.
+ *
+ * 입력
+ * 첫 줄에 테스트케이스의 수가 주어진다. 각 테스트케이스는 두 줄로 이루어져 있다.
+ *
+ * 테스트케이스의 첫 번째 줄에는 문서의 개수 N(1 ≤ N ≤ 100)과, 몇 번째로 인쇄되었는지 궁금한 문서가 현재 Queue에서 몇 번째에 놓여 있는지를 나타내는 정수 M(0 ≤ M < N)이 주어진다. 이때 맨 왼쪽은 0번째라고 하자. 두 번째 줄에는 N개 문서의 중요도가 차례대로 주어진다. 중요도는 1 이상 9 이하의 정수이고, 중요도가 같은 문서가 여러 개 있을 수도 있다.
+ *
+ * 출력
+ * 각 테스트 케이스에 대해 문서가 몇 번째로 인쇄되는지 출력한다.
+ */
+public class 프린터큐 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            LinkedList<int[]> queue = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                // {초기 위치, 중요도}
+                queue.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
+            }
+
+            int count = 0;	// 출력 횟수
+            while (!queue.isEmpty()) {
+                int[] front = queue.poll();	// 가장 첫 원소
+                boolean isMax = true;	// front 원소가 가장 큰 원소인지를 판단하는 변수
+
+                // 큐에 남아있는 원소들과 중요도를 비교
+                for(int i = 0; i < queue.size(); i++) {
+                    // 처음 뽑은 원소보다 큐에 있는 i번째 원소가 중요도가 클 경우
+                    if(front[1] < queue.get(i)[1]) {
+                        // 뽑은 원소 및 i 이전의 원소들을 뒤로 보낸다.
+                        queue.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            queue.offer(queue.poll());
+                        }
+                        // front원소가 가장 큰 원소가 아니였으므로 false를 하고 탐색을 마침
+                        isMax = false;
+                        break;
+                    }
+                }
+
+                // front 원소가 가장 큰 원소가 아니였으므로 다음 반복문으로 넘어감
+                if(isMax == false) {
+                    continue;
+                }
+
+                // front 원소가 가장 큰 원소였으므로 해당 원소는 출력해야하는 문서다.
+                count++;
+                if(front[0] == M) {	// 찾고자 하는 문서라면 해당 테스트케이스 종료
+                    break;
+                }
+
+            }
+
+            sb.append(count).append('\n');
+
+        }
+        System.out.println(sb);
+    }
+}
